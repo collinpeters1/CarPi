a39411bb50766e7edd3084089748c672b38099ae
StuartRayP <plaugherstuart@yahoo.com>
2025-07-20 16:57:06 -0500

    Fixed Syntax error

--- motor_lib.py
+++ motor_lib.py
@@ -14,50 +14,50 @@ p = None
 
 ##### FUNCTION LIB ###########
 
-    def setup_pins():
-        """initialize GPIO"""
-        global p
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(MOTOR_IN1_PIN, GPIO.OUT)
-        GPIO.setup(MOTOR_IN2_PIN, GPIO.OUT)
-        GPIO.setup(ENABLE_PIN, GPIO.OUT)
-        
-        p = GPIO.PWM(ENABLE_PIN, PWM_FREQUENCY)
-        print("GPIO enabled --- you are clear for launch")
-        
+def setup_pins():
+    """initialize GPIO"""
+    global p
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(MOTOR_IN1_PIN, GPIO.OUT)
+    GPIO.setup(MOTOR_IN2_PIN, GPIO.OUT)
+    GPIO.setup(ENABLE_PIN, GPIO.OUT)
     
-    def motor_forward(speed):
+    p = GPIO.PWM(ENABLE_PIN, PWM_FREQUENCY)
+    print("GPIO enabled --- you are clear for launch")
+    
+
+def motor_forward(speed):
+    GPIO.output(MOTOR_IN1_PIN, GPIO.HIGH)
+    GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
+    # https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+    p.start(speed)
+    print(f"Motor moving forward at {speed}% speed.")
+
+def motor_backward(speed):
+    GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
+    GPIO.output(MOTOR_IN2_PIN, GPIO.HIGH)
+    # https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+    p.start(speed)
+    print(f"Motor moving backward at {speed}% speed.") 
+    
+def motor_brake():
+    p.stop()
+    GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
+    GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
+    
+
+
+def motor_drive(direction, speed):
+    """
+    Drives the motor in a specific direction at a given speed.
+    :param direction: grow position - g, load position - l
+    :param speed: int (0-100), the duty cycle for motor speed.
+    """
+    if direction == 'g':
         GPIO.output(MOTOR_IN1_PIN, GPIO.HIGH)
         GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
-        # https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
         p.start(speed)
-        print(f"Motor moving forward at {speed}% speed.")
- 
-    def motor_backward(speed):
+    elif direction == 'l':
         GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
         GPIO.output(MOTOR_IN2_PIN, GPIO.HIGH)
-        # https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
         p.start(speed)
-        print(f"Motor moving backward at {speed}% speed.") 
-        
-    def motor_brake():
-        p.stop()
-        GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
-        GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
-        
-
-    
-    def motor_drive(direction, speed):
-        """
-        Drives the motor in a specific direction at a given speed.
-        :param direction: grow position - g, load position - l
-        :param speed: int (0-100), the duty cycle for motor speed.
-        """
-        if direction == 'g':
-            GPIO.output(MOTOR_IN1_PIN, GPIO.HIGH)
-            GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
-            p.start(speed)
-        elif direction == 'l':
-            GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
-            GPIO.output(MOTOR_IN2_PIN, GPIO.HIGH)
-            p.start(speed)
